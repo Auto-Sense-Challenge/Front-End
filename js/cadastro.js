c1ab986134f7e8ab14ecd31fbbec22bc5a33fc54
fFukurou <supevi1@gmail.com>
2024-05-24 23:56:21 -0300

    consertado botao

--- js/cadastro.js
+++ js/cadastro.js
@@ -2,15 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     var form = document.querySelector(".inputs");
     document.getElementById('btnEnviar').addEventListener('click', function(event) {
         event.preventDefault();
-      
-        var nomeUsuario = document.querySelector("#campoNomeUser").value;
-        var nomeCompleto = document.querySelector("#campoNome").value;
-        var senha = document.querySelector("#capmoSenha").value;
-        var email = document.querySelector("#campoEmail").value;
+
+        document.getElementById('cadastroButton').textContent = "Dados Enviados!";
+
+        var nomeUsuario = document.getElementById('txtfield1').value;
+        var nomeCompleto = document.getElementById('txtfield2').value;
+        var senha = document.getElementById('txtfield3').value;
+        var email = document.getElementById('txtfield4').value;
+
+        document.getElementById('btnEnviar')
 
         console.log('Nome de usuário:', nomeUsuario);
         console.log('Nome completo:', nomeCompleto);
-        console.log('Senha:', senha);
+        console.log('Senha:', '**senha**');
         console.log('E-mail:', email);
 
 
