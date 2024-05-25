afd45ec93b1e340ae57c503ec870073789b0afbc
fFukurou <supevi1@gmail.com>
2024-05-24 23:54:18 -0300

    abutton

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     var form = document.querySelector(".inputs");
-    form.addEventListener("submit", function(event) {
+    document.getElementById('btnEnviar').addEventListener('click', function(event) {
         event.preventDefault();
-
+      
         var nomeUsuario = document.querySelector("#campoNomeUser").value;
         var nomeCompleto = document.querySelector("#campoNome").value;
         var senha = document.querySelector("#capmoSenha").value;
         var email = document.querySelector("#campoEmail").value;
 
-        if (nomeUsuario === "" || nomeCompleto === "" || senha === "" || email === "") {
-            alert("Por favor, preencha todos os campos obrigatórios.");
-            return;
-        }
-        alert("Formulário enviado com sucesso!");
+        console.log('Nome de usuário:', nomeUsuario);
+        console.log('Nome completo:', nomeCompleto);
+        console.log('Senha:', senha);
+        console.log('E-mail:', email);
+
+
     });
 });
