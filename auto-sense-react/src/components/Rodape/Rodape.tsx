17ab709249a63a9b2c9a0409951f3f3199a17d87
LuizHNR <luizhneri12@gmail.com>
2024-09-12 22:44:42 -0300

    footer

--- auto-sense-react/src/components/Rodape/Rodape.tsx
+++ auto-sense-react/src/components/Rodape/Rodape.tsx
@@ -6,17 +6,17 @@ export default function Rodape(){
     return(
         <footer>
             <div className="esquerda">
-                <div className="auto-sense">
-                    <img src={logo} alt="auto-sense-logo" />
+                <div className="auto-sense-footer">
+                    <img src={logo} alt="auto-sense-logo-footer" />
                     <p>Auto Sense</p>
                 </div>
                 <img src={git} alt="github-logo" />
             </div>
-                    <ul>
-                        <li><Link to={"/Sobre"}>Quem Somos</Link></li>
-                        <li><Link to="/Cadastro">Cadastre-se</Link></li>
-                        <li>Entre em contato</li>
-                    </ul>
+                <ul>
+                    <li>Entre em contato</li>
+                    <li><Link to={"/Sobre"}>Quem Somos</Link></li>
+                    <li><Link to="/Cadastro">Cadastre-se</Link></li>    
+                </ul>
             <div className="copy">
                 <p>&copy; 2024 Auto Sense</p>
             </div>
