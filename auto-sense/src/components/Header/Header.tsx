8606c99677e9ce31d549c76aa7a946558275aca9
Erick0105 <erickaxs0105@gmail.com>
2024-10-11 08:06:52 -0300

    header criado com sucesso

--- auto-sense/src/components/Header/Header.tsx
+++ auto-sense/src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
+"use client"
+
 import Link from 'next/link'
 import React, { useState } from 'react'
 import Image from 'next/image'
 import AutoLogo from "@/img/auto-logo.png"
 import Menu from "@/img/menu-img.png"
 import UserFoto from "@/img/foto-user.png"
+import Nav from '../Nav/Nav'
 
 export default function Header() {
 
@@ -34,7 +37,7 @@ export default function Header() {
         </div>
 
         <div className='abrirMenu'>
-            <button>
+            <button onClick={() => mudarNav()}>
                 <Image src={Menu} alt='Img-menu'/>
             </button>
         </div>
@@ -44,10 +47,30 @@ export default function Header() {
                 <Image src={UserFoto} alt="foto-usuario"/>
             </div>
 
-            <button>
+            <button onClick={() => mudarOpcoes()}>
                 <p>&gt;</p>
             </button>
         </div>
+
+        <dialog open={navStatus} className='dialogNav'>
+            <Nav/>
+        </dialog>
+
+        <dialog open={opcoesStatus} className='dialogOpcoes'>
+            <div className="opcoesUser">
+                <Image src={UserFoto} alt="foto-usuario"/>
+                <h5>Olá, Bem vindo(a)</h5>
+                <div className="op-cadastros">
+                    <Link href="/cadastrar/usuario">Cadastrar Usuário</Link>
+                    <Link href="/cadastrar/mecanico">Cadastrar Mecânico</Link>
+                    <Link href="/login">Login</Link>
+                </div>
+                <div className="paginas">
+                    <Link href="/ajuda">Ajuda</Link>
+                    <Link href="/sobre">Sobre</Link>
+                </div>
+            </div>
+        </dialog>
     </header>
   )
 }
