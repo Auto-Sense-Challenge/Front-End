dec4af020d05d4e8fd2f5b2d4d999af6dff63bc9
Erick0105 <erickaxs0105@gmail.com>
2024-10-18 11:07:27 -0300

    ta melhor

--- auto-sense/src/app/mecanicos/mecanico/[id]/page.tsx
+++ auto-sense/src/app/mecanicos/mecanico/[id]/page.tsx
@@ -16,7 +16,14 @@ export default function Mecanico({params}:{params:{id:number}}) {
     <main>
         {/* Essa página é a página que os usuarios veram os dados de um mecânico expecifico*/}
         
-        
+        <h3>Nome: {mecanico.nome}</h3>
+        <ul>
+          <li>Telefone: {mecanico.telefone}</li>
+          <li>Endereço: {mecanico.endereco}</li>
+        </ul>
+
+        <h4>Avaliação</h4>
+        <h6>{mecanico.avaliacao}</h6>
     </main>
   )
 
